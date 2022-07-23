49bb3812087eebe983d789969a608b9a43d33989
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-07-23 08:41:33 +0100

    added  new property

--- VisitorManagementModel/Model/Employee.cs
+++ VisitorManagementModel/Model/Employee.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public Role Role { get; set; }
+        public Department Department {get;set;}
     }
 }
