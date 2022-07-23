66277c0c8f4d5e15491b5b4b0f0898fbf8e1a518
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-07-23 07:07:24 +0100

    added notification model

---
+++ VisitorManagementModel/Model/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorManagementModel.Model
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public Visitor Visitor { get; set; }
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+    }
+}
